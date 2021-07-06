6c3c1be1e0305311b44b644fcbe0553bf9a95ef1
Emir Melih Erdem <melih.erdem@ug.bilkent.edu.tr>
2021-07-06 14:02:32 +0300

    Add project files.

---
+++ WhatToDo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhatToDo.Startup))]
+namespace WhatToDo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
