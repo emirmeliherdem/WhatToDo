b5079e670d9fc43aac160332e9a2bb54daf7306d
Emir Melih Erdem <melih.erdem@ug.bilkent.edu.tr>
2021-07-07 18:21:24 +0300

    Direct Delete

--- WhatToDo/Controllers/HomeController.cs
+++ WhatToDo/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace WhatToDo.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +22,7 @@ namespace WhatToDo.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
